7ed68ce4bccf521cccb5a8a68e4c04c68e3da772
yangjh-xbmu <yangjh@yeah.net>
2023-09-14 10:49:09 +0800

    zidong

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name 'yangjh-xbmu'
 git config --global user.email 'yangjh@yeah.net'
 git config --global http.sslverify false
 git add .
-git commit -m '自动推送'
+git commit -m zidong
 git push
