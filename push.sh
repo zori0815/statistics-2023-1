33e6263f082d4521d77f6fbc709947300196708c
yangjh-xbmu <yangjh@yeah.net>
2023-10-07 10:39:05 +0800

    autopush

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.name 'yangjh-xbmu'
 git config --global user.email 'yangjh@yeah.net'
 git config --global http.sslverify false
 git add .
-git commit -m zidong
+git commit -m "autopush"
 git push
