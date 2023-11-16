da9ce4b099222367de8da435156991825f41a51d
yangjh-xbmu <yangjh@yeah.net>
2023-11-16 10:08:16 +0800

    autopush

---
+++ mytools.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams["font.sans-serif"] = ["SimHei"]  # 设置字体
+
+
+def 有序变量描述统计函数(表名, 变量名):
+    result = 表名[变量名].value_counts(sort=False)
+    描述统计表 = pd.DataFrame(result)
+    描述统计表['比例'] = 描述统计表['count'] / 描述统计表['count'].sum()
+    描述统计表['累计比例'] = 描述统计表['比例'].cumsum()
+    return 描述统计表
+
+
+def 绘制直方图(表名):
+    x = 表名.index
+    y = 表名['count'].values
+    fig, ax2 = plt.subplots()
+    ax2.bar(x, y)
+    plt.show()
